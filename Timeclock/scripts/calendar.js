ac3a5f8797e9174ad02f5b536ca7413af1ebbe2b
Tyler Camp <algorithmbandit@yahoo.com>
2013-06-04 00:53:14 -0500

    Calendar can generate the row/data sets for individual days

--- Timeclock/scripts/calendar.js
+++ Timeclock/scripts/calendar.js
@@ -3,9 +3,25 @@ function Calendar(
     element /* jQuery object */
     )
 {
-    this.instance = element;
+    this.element = element;
     
-    element.load( "scripts/calendartemplate.html" );
+    element.load( "scripts/calendartemplate.html", function () {
+        var headElement = element.find( "thead" );
+        var dayRow = $( "<tr>" );
+        dayRow.append( "<td>Sun</td>" );
+        dayRow.append( "<td>Mon</td>" );
+        dayRow.append( "<td>Tue</td>" );
+        dayRow.append( "<td>Wed</td>" );
+        dayRow.append( "<td>Thu</td>" );
+        dayRow.append( "<td>Fri</td>" );
+        dayRow.append( "<td>Sat</td>" );
+        headElement.append( dayRow );
+        
+        for( var i = 0; i < 7; i++ )
+        {
+            var weekRow = tableElement.append( "<tr>" );
+        }
+    });
     
     this.addColumn = function( )
     {
